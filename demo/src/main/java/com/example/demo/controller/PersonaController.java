80981b9ca5a06749cc2f3aea8ffc738e42de0d27
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-08-25 12:16:37 -0500

    modelo de persona

---
+++ demo/src/main/java/com/example/demo/controller/PersonaController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.IPersonaService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/personas")
+public class PersonaController {
+
+    private final IPersonaService personaService;
+
+    public PersonaController(IPersonaService personaService) {
+        this.personaService = personaService;
+    }
+
+
+
+}
---
+++ demo/src/main/java/com/example/demo/model/Persona.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "personas")
+@Getter
+@Setter
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_medico")
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "edad")
+    private int edad;
+}
